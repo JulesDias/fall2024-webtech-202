d17f1b87a6c1d6440e0f73988a599dd973146cd3
JulesDias <julesdias92@gmail.com>
2024-09-30 11:10:01 +0200

    lab 1

---
+++ handle.js
@@ -0,0 +1,27 @@
+// ./handle.js
+
+const url = require('url');
+const qs = require('querystring');
+
+module.exports = {
+  serverHandle: function (req, res) {
+    const route = url.parse(req.url);
+    const path = route.pathname;
+    const params = qs.parse(route.query);
+
+    // Log the path and query parameters
+    console.log(path);
+    console.log(params);
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+
+    // Respond based on the path and query params
+    if (path === '/hello' && 'name' in params) {
+      res.write('Hello ' + params['name']);
+    } else {
+      res.write('Hello anonymous');
+    }
+
+    res.end();
+  }
+};
---
+++ index.js
@@ -0,0 +1,9 @@
+// ./index.js
+
+const http = require('http');
+const handles = require('./handle');
+
+const server = http.createServer(handles.serverHandle);
+server.listen(8080, () => {
+  console.log('Server is listening on port 8080');
+});

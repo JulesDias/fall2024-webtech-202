611a786088d8b45b5fc4e647664789485990654e
JulesDias <julesdias92@gmail.com>
2024-10-02 12:33:41 +0200

    feat: update db file

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const http = require('http');
 const handles = require('./handle');
+const db = require('./db');
+
+
 
 const server = http.createServer(handles.serverHandle);
 server.listen(8080, () => {
   console.log('Server is listening on port 8080');
 });
+

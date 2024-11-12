9a92854e6b8e03997a29b44aeb6745e8608267d3
Arthur B. <a.berret.hnlt@gmail.com>
2024-11-12 03:41:16 +0100

    wip: about page parallax

--- client/src/app/about/page.js
+++ client/src/app/about/page.js
@@ -1,17 +1,34 @@
 // About page
 
+"use client"
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import {useRef} from 'react';
+import React from "react";
 
 export default function About() {
+
+    
+  //var parallax
+  const ref = useRef();
+
+
   return (
     
-        <div className="max-w-2xl">
-          <h1 className="wt-title-better">
-            Here's What We're About
-          </h1>
-          <p className="mb-8 text-lg text-gray-900 font-roboto dark:text-gray-200">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          </p>
-        </div>
+    <Parallax pages={3} ref={ref} className='hide-scrollbar'>
+
+    {/*background planet component */}
+    <ParallaxLayer offset={0.5} factor={3}>
+      
+    </ParallaxLayer>
+
+    {/*Head text*/}
+    <ParallaxLayer offset={0.20} factor={0.5} className="mx-auto text-center max-w-1/3 " >
+    <h1 className="text-5xl  font-extrabold text-black dark:text-white font-FS_Sinclair">
+      About the Democratic Liberty Hub
+    </h1>
+    </ParallaxLayer>
+
+    </Parallax>
     
   );
 }

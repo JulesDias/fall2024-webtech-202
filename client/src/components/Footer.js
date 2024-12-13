43eb23c663f2254ae5b364b97f05d012d51b4043
JulesDias <julesdias92@gmail.com>
2024-12-13 16:41:47 +0100

    feat: article profile page upgrade

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,11 +1,11 @@
-import { DarkModeProvider, useDarkMode  } from '../components/DarkmodeContext';
+import { useDarkMode } from '../components/DarkmodeContext';
 
 export default function Footer() {
 
-  const { isDarkMode } = useDarkMode();  // Access dark mode value from context
-  
+  useDarkMode();  // Access dark mode value from context
+
   return (
-    <footer className="p-6 text-center bg-gray-400 text-white dark:bg-gray-700 dark:text-gray-400" >
+    <footer className="footer p-6 text-center bg-gray-400 text-white dark:bg-gray-700 dark:text-gray-400">
       <p className="text-sm">© 2024 Democratic Liberty Hub - All Rights Reserved</p>
     </footer>
   );
